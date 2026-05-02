95341d2375202d8d82aba3c27db4f7361b77c620
Haritha Prageeth <134475404+HarithaPrageeth@users.noreply.github.com>
2026-05-02 23:05:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import ProjectDetails from './components/ProjectDetails';
 import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
-      <div className="bg-gray-900">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={
-            <>
-              <Hero />
-              <About />
-              <Skills />
-              <Projects />
-              <Contact />
-            </>
-          } />
-          <Route path="/project/:id" element={<ProjectDetails />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="bg-gray-900">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
   );
 }
 
